ecee9e621c8adc0ef21b79248ddb71df550ba31c
heyliceeee <alicedias2002@hotmail.com>
2026-05-05 15:55:52 +0100

    ✨feat: move left, right and clear draw

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from turtle import Turtle, Screen
+from turtle import Turtle, Screen, mode
 
 tim = Turtle()
 screen = Screen()
@@ -14,13 +14,30 @@ def move_backwards():
     turtle move backward 10 steps
     """
     tim.backward(10)
-
+def move_left():
+    """
+    turtle move left
+    """
+    tim.left(10)
+def move_right():
+    """
+    turtle move right
+    """
+    tim.right(10)
+def clear_drawing():
+    """
+    clear draw and put turtle in center
+    """
+    tim.clear() # clear the draw
+    tim.penup() # no draw while go center
+    tim.home() # turtle go center
+    tim.pendown() # lets draw while move
 
 screen.listen()
-screen.onkey(key="w", fun=move_forwards) # when click in w key, turtle move forward
-screen.onkey(key="s", fun=move_backwards) # when click in s key, turtle move backward
-screen.onkey(key="a", fun=move_counter_clockwise) # when click in a key, turtle move counter-clockwise
-screen.onkey(key="d", fun=move_clockwise) # when click in d key, turtle move clockwise
-screen.onkey(key="c", fun=clear_drawing) # when click in c key, clear draw
+screen.onkey(move_forwards, "w") # when click in w key, turtle move forward
+screen.onkey(move_backwards, "s") # when click in s key, turtle move backward
+screen.onkey(move_left, "a") # when click in a key, turtle move left
+screen.onkey(move_right, "d") # when click in d key, turtle move right
+screen.onkey(clear_drawing, "c") # when click in c key, clear draw
 
 screen.exitonclick() # screen close only if you click him
